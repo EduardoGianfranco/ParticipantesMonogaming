1e14f26fe79f5e43afbe5c9dda8a455b15eae0eb
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-08-27 05:25:51 -0500

    firzth commit

---
+++ src/main/java/com/acme/demoapi/model/Participante.java
@@ -0,0 +1,18 @@
+package com.acme.demoapi.model;
+
+import java.util.Date;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Participante {
+    private String id;
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private Date birthDate;
+}
