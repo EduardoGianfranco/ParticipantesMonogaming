87dcc1f332b69ee98e04135f572cd5e9c45346e0
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-09-20 02:56:59 -0500

    Add detallesFactura Conexion ManyToOne - OneToMany

--- src/main/java/com/acme/demoapi/model/Factura.java
+++ src/main/java/com/acme/demoapi/model/Factura.java
@@ -2,14 +2,11 @@ package com.acme.demoapi.model;
 
 import java.math.BigDecimal;
 import java.sql.Date;
-
 import javax.persistence.GeneratedValue;
-
 import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
-
 import lombok.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -29,4 +26,6 @@ public class Factura {
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date fechaFactura;
     private BigDecimal totalFactura;
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<DetalleFactura> detalleFacturas;  //Realizar conexión con DetalleFactura
 }
