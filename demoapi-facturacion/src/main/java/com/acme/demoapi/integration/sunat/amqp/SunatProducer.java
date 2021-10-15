eac08be4dd7bc5ad0fb0712e1eb46b7ea5d39f1a
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-10-15 01:47:51 -0500

    AMQP consumer x2

--- demoapi-facturacion/src/main/java/com/acme/demoapi/integration/sunat/amqp/SunatProducer.java
+++ demoapi-facturacion/src/main/java/com/acme/demoapi/integration/sunat/amqp/SunatProducer.java
@@ -3,6 +3,8 @@ package com.acme.demoapi.integration.sunat.amqp;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.acme.demoapi.integration.sunat.dto.Invoice;
 import com.acme.demoapi.model.Factura;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,7 +23,13 @@ public class SunatProducer {
     public void send(Factura facturaContoso){   //Recibe el parametro factura
         try{
             ObjectMapper mapper = new ObjectMapper();   //Crea un mapper hacia json
-            String json = mapper.writeValueAsString(facturaContoso);    //convierte la factura a json que es un string
+            Invoice payload = new Invoice();
+            payload.setFechaEmision(facturaContoso.getFechaFactura());
+            payload.setImporte(facturaContoso.getTotalFactura());
+            payload.setRucEmisor("999999999");
+            payload.setRucComprador(facturaContoso.getCodigoCliente());
+            
+            String json = mapper.writeValueAsString(payload);    //convierte la ~factura~ PAYLOAD a json que es un string
             amqpTemplate.convertSendAndReceive(EXCHANGE_NAME, "", json);    //Se envía el mensaje (json) a amqp
         }catch(JsonProcessingException e){
             e.printStackTrace();
